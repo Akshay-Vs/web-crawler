c4982df987834938d8a9cf57510d1dfb0c535b4f
Akshay Vs <me.akshay.vs@gmail.com>
2024-06-16 21:06:02 +0530

    feat: added crawler

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("hello world") 
+const { crawlPage } = require('./crawl/crawl')
+
+function main() {
+  if (process.argv.length < 3) {
+    console.log("No website provided")
+    process.exit(1)
+  }
+
+  if (process.argv.length > 3) {
+    console.log("Too many arguments")
+    process.exit(1)
+  }
+
+  const baseUrl = process.argv[2]
+
+  console.log(`Crawling '${baseUrl}'...`)
+  crawlPage(baseUrl)
+}
+
+main()
